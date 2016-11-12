4acc6449bc245cf70aab34a2df565837a95264eb
joffrey <joffrey.armellini@gmail.com>
2016-11-12 15:31:44 -0500

    1st button added

--- tradeit/SecondViewController.swift
+++ tradeit/SecondViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    // Outlet reference to the "Log +1 in Firebase!" button
+    @IBOutlet weak var log1InFirebase: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
+        
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    // Action called when "Log +1 in Firebase!" button pressed
+    @IBAction func log1InFirebasePressed(sender: UIButton) {
+        // Do something
+        print("'Log +1 in Firebase!' button pressed")
+    }
 
 }
 
