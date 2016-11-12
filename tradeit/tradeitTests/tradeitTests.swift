0bc34feda7b14381b14396df18a442086e230b64
joffrey <joffrey.armellini@gmail.com>
2016-11-12 14:56:38 -0500

    First Commit

---
+++ tradeit/tradeitTests/tradeitTests.swift
@@ -0,0 +1,36 @@
+//
+//  tradeitTests.swift
+//  tradeitTests
+//
+//  Created by Joffrey Armellini on 2016-11-12.
+//  Copyright © 2016 Joffrey Armellini. All rights reserved.
+//
+
+import XCTest
+@testable import tradeit
+
+class tradeitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
