a1918246895a93873299d578db41e62e37956cee
joffrey <joffrey.armellini@gmail.com>
2016-12-08 19:31:31 -0500

    ItemsArray model class working and observer concept as well for init completion

--- tradeit/FirstViewController.swift
+++ tradeit/FirstViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import Firebase
 
 class FirstViewController: UIViewController {
 
+    var myContext = 0
+    let testRef: FIRDatabaseReference! = FIRDatabase.database().reference()
+    var testItemsArray: ItemsArray!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.testItemsArray = ItemsArray(withMetadataFromFBRef: testRef)
+        
+        // Put an observer on the items array "initComplted" key
+        testItemsArray.addObserver(self, forKeyPath: "metadataInitCompleted", options: .new, context: &myContext)
+        
     }
 
+    //override the observer function
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        print("Observer triggered!")
+        
+        if context == &myContext {
+            print("items array init completed: \(change?[NSKeyValueChangeKey.newKey])")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
---
+++ tradeit/ItemsArray.swift
@@ -0,0 +1,63 @@
+import Foundation
+import Firebase
+
+class ItemsArray: NSObject {
+    // content (array itself)
+    var content: [Item] = []
+    // to monitor when metadata init process is completed (depending on FB data reading) ("dynamic" enables observation)
+    dynamic var metadataInitCompleted = false
+    
+    // Initializer
+    init(withMetadataFromFBRef ref: FIRDatabaseReference) {
+        
+        // Call up to NSObject initializer
+        super.init()
+        
+        // read Firebase data once and fill the content based on data retrieved
+        
+        ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            // create a NSDictionary with all elements in the snasphot of the ref
+            let snapshotDictionary = snapshot.value as? NSDictionary
+            
+            // if that snapshotDictionary is not nil, use it
+            if let dictionary = snapshotDictionary {
+                
+                // For each element in the dictionary, create the corresponding Item object and append it to the self.content Array
+                for element in dictionary {
+                    
+                    print("Entering the loop to create 'ItemsArray' object based on firebase element:")
+                    print(element.value)
+                    
+                    // element NSDictionary
+                    let elementNSDictionary = element.value as! NSDictionary
+                    
+                    // new Item object
+                    let itemToAppend = Item()
+                    
+                    // Populate the itemToAppend with FB Database element values
+                    itemToAppend.key = elementNSDictionary["key"] as? String
+                    itemToAppend.description = elementNSDictionary["description"] as? String
+                    itemToAppend.tags = elementNSDictionary["description"] as? [String]
+                    itemToAppend.imagePath = elementNSDictionary["imagePath"] as? String
+                    itemToAppend.imageThumbnailPath = elementNSDictionary["imageThumbnailPath"] as? String
+                    print("For item \(itemToAppend.key): Successfully set properties: key, description, tags, imagePath, imageThumbnailPath")
+                    
+                    // Append the item to the array
+                    self.content.append(itemToAppend)
+                    print("For item \(itemToAppend.key): Appended the item to the itemsArray.content")
+                    
+                }
+                print("Items Array metadata initialization is now completed! with \(self.content.count) elements")
+                self.metadataInitCompleted = true
+                
+            }
+        })
+        { (error) in
+            print("Oops, following errror occured while trying to build ItemsArray object metadata from FB: \(error.localizedDescription)")
+        }
+        
+        
+    } // END of init()
+    
+}
