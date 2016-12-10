015b399e36a21a0f53bef580ea25059323e80c3e
joffrey <joffrey.armellini@gmail.com>
2016-12-10 18:34:50 -0500

    item display working just fine

--- tradeit/ItemDisplayViewController.swift
+++ tradeit/ItemDisplayViewController.swift
@@ -10,12 +10,29 @@ class ItemDisplayViewController: UIViewController {
     // MARK: Properties
     var itemToDisplay: Item?
     
+    // Firebase storage reference
+    let imagesRef = FIRStorage.storage().reference(forURL: "gs://tradeit-99edf.appspot.com/").child("images")
+    
+    // MARK: METHODS
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("Display item data in the view")
-        self.itemImage.image = self.itemToDisplay?.image
+        print("Display item description in the view")
         self.itemDescription.text = self.itemToDisplay?.description
+        
+        print("Start loading of item image")
+        self.itemToDisplay?.downloadImage(kind: .original, atFBStorageRef: imagesRef) { error in
+            // completion block
+            if error == nil {
+                print("item image downloaded successfully!")
+                print("set the image in the view!")
+                self.itemImage.image = self.itemToDisplay?.image
+            } else {
+                print("item image download failed with error: \(error)")
+            }
+            
+        }
         
         
     }
